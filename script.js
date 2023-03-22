7f5b35bc6826fdfcaaf804217062aa78cb422f9f
jsots <justinsotolongo@gmail.com>
2023-03-22 15:27:27 -0400

    Complete with a tie, but the game still adds the last color after the alert

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let topR = document.querySelectorAll(".topR")
 let midR = document.querySelectorAll(".midR")
 let botR = document.querySelectorAll(".botR")
 let gameB = [topR, midR, botR]
+let w = 0
+let c = 0
 
 function switchP() {
     if (currentP === "p1") {
@@ -19,41 +21,61 @@ boxes.forEach ((box) => {
         box.classList.add(currentP)
         switchP()
         checkWinner()
+        c++
+        if (c === 9 && w === 0) {
+            alert("It's a tie!")
+        }
     })
 })
 
 function checkWinner() {
     if (gameB[0][0].classList.contains("p1") && gameB[1][0].classList.contains("p1") && gameB[2][0].classList.contains("p1")) {  // Vertical win
+        w = 1
         alert("P1 wins!")
     } else if (gameB[0][1].classList.contains("p1") && gameB[1][1].classList.contains("p1") && gameB[2][1].classList.contains("p1")) {
+        w = 1
         alert("P1 wins!")
     } else if (gameB[0][2].classList.contains("p1") && gameB[1][2].classList.contains("p1") && gameB[2][2].classList.contains("p1")) {
+        w = 1
         alert("P1 wins!")
     } else if (gameB[0][0].classList.contains("p1") && gameB[1][1].classList.contains("p1") && gameB[2][2].classList.contains("p1")) {
+        w = 1
         alert("P1 wins!")
     } else if (gameB[0][2].classList.contains("p1") && gameB[1][1].classList.contains("p1") && gameB[2][0].classList.contains("p1")) {
+        w = 1
         alert("P1 wins!")
     } else if (gameB[0][0].classList.contains("p2") && gameB[1][0].classList.contains("p2") && gameB[2][0].classList.contains("p2")) {
+        w = 1
         alert("P2 wins!")
     } else if (gameB[0][1].classList.contains("p2") && gameB[1][1].classList.contains("p2") && gameB[2][2].classList.contains("p2")) {
+        w = 1
         alert("P2 wins!")
     } else if (gameB[0][2].classList.contains("p2") && gameB[1][2].classList.contains("p2") && gameB[2][2].classList.contains("p2")) {
+        w = 1
         alert("P2 wins!")
     } else if (gameB[0][0].classList.contains("p2") && gameB[1][1].classList.contains("p2") && gameB[2][2].classList.contains("p2")) {
+        w = 1
         alert("P2 wins!")
     } else if (gameB[0][2].classList.contains("p2") && gameB[1][1].classList.contains("p2") && gameB[2][0].classList.contains("p2")) {
+        w = 1
         alert("P2 wins!")
     } else if (gameB[0][0].classList.contains("p1") && gameB[0][1].classList.contains("p1") && gameB[0][2].classList.contains("p1")) {
+        w = 1
         alert("P1 wins!")
     } else if (gameB[1][0].classList.contains("p1") && gameB[1][1].classList.contains("p1") && gameB[1][2].classList.contains("p1")) {
+        w = 1
         alert("P1 wins!")
     } else if (gameB[2][0].classList.contains("p1") && gameB[2][1].classList.contains("p1") && gameB[2][2].classList.contains("p1")) {
+        w = 1
         alert("P1 wins!")
     } else if (gameB[0][0].classList.contains("p2") && gameB[0][1].classList.contains("p2") && gameB[0][2].classList.contains("p2")) {
+        w = 1
         alert("P2 wins!")
     } else if (gameB[1][0].classList.contains("p2") && gameB[1][1].classList.contains("p2") && gameB[1][2].classList.contains("p2")) {
+        w = 1
         alert("P2 wins!")
     } else if (gameB[2][0].classList.contains("p2") && gameB[2][1].classList.contains("p2") && gameB[2][2].classList.contains("p2")) {
+        w = 1
         alert("P2 wins!")
     } else {
         console.log("Why are you looking over here? Play the game!")
