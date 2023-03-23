0b58e96dd19d52ab6a3fec8a5ead52343d7e202c
jsots <justinsotolongo@gmail.com>
2023-03-22 20:31:57 -0400

    Almost done, just need to fix the spacing for the scores and then make it phone compatible maybe

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ let botR = document.querySelectorAll(".botR")
 let h3 = document.querySelector("h3")
 let gameB = [topR, midR, botR]
 let clearB = document.querySelector("button")
+let p1Score = 0
+let p2Score = 0
+let p1Val = document.querySelector(".p1Val")
+let p2Val = document.querySelector(".p2Val")
 let w = 0
 let c = 0
 
@@ -37,66 +41,162 @@ function checkWinner() {
     if (gameB[0][0].classList.contains("p1") && gameB[1][0].classList.contains("p1") && gameB[2][0].classList.contains("p1")) {  // Vertical win
         w = 1
         h3.innerText = "P1 wins!"
+        p1Score += 1
+        if (p1Score < 10) {
+            p1Val.innerText = "0" + p1Score
+        } else {
+            p1Val.innerText = p1Score
+        }
         return true
     } else if (gameB[0][1].classList.contains("p1") && gameB[1][1].classList.contains("p1") && gameB[2][1].classList.contains("p1")) {
         w = 1
         h3.innerText = "P1 wins!"
+        p1Score += 1
+        if (p1Score < 10) {
+            p1Val.innerText = "0" + p1Score
+        } else {
+            p1Val.innerText = p1Score
+        }
         return true
     } else if (gameB[0][2].classList.contains("p1") && gameB[1][2].classList.contains("p1") && gameB[2][2].classList.contains("p1")) {
         w = 1
         h3.innerText = "P1 wins!"
+        p1Score += 1
+        if (p1Score < 10) {
+            p1Val.innerText = "0" + p1Score
+        } else {
+            p1Val.innerText = p1Score
+        }
         return true
     } else if (gameB[0][0].classList.contains("p1") && gameB[1][1].classList.contains("p1") && gameB[2][2].classList.contains("p1")) {
         w = 1
         h3.innerText = "P1 wins!"
+        p1Score += 1
+        if (p1Score < 10) {
+            p1Val.innerText = "0" + p1Score
+        } else {
+            p1Val.innerText = p1Score
+        }
         return true
     } else if (gameB[0][2].classList.contains("p1") && gameB[1][1].classList.contains("p1") && gameB[2][0].classList.contains("p1")) {
         w = 1
         h3.innerText = "P1 wins!"
+        p1Score += 1
+        if (p1Score < 10) {
+            p1Val.innerText = "0" + p1Score
+        } else {
+            p1Val.innerText = p1Score
+        }
         return true
     } else if (gameB[0][0].classList.contains("p2") && gameB[1][0].classList.contains("p2") && gameB[2][0].classList.contains("p2")) {
         w = 1
         h3.innerText = "P2 wins!"
+        p2Score += 1
+        if (p2Score < 10) {
+            p2Val.innerText = "0" + p2Score
+        } else {
+            p2Val.innerText = p2Score
+        }
         return true
     } else if (gameB[0][1].classList.contains("p2") && gameB[1][1].classList.contains("p2") && gameB[2][1].classList.contains("p2")) {
         w = 1
         h3.innerText = "P2 wins!"
+        p2Score += 1
+        if (p2Score < 10) {
+            p2Val.innerText = "0" + p2Score
+        } else {
+            p2Val.innerText = p2Score
+        }
         return true
     } else if (gameB[0][2].classList.contains("p2") && gameB[1][2].classList.contains("p2") && gameB[2][2].classList.contains("p2")) {
         w = 1
         h3.innerText = "P2 wins!"
+        p2Score += 1
+        if (p2Score < 10) {
+            p2Val.innerText = "0" + p2Score
+        } else {
+            p2Val.innerText = p2Score
+        }
         return true
     } else if (gameB[0][0].classList.contains("p2") && gameB[1][1].classList.contains("p2") && gameB[2][2].classList.contains("p2")) {
         w = 1
         h3.innerText = "P2 wins!"
+        p2Score += 1
+        if (p2Score < 10) {
+            p2Val.innerText = "0" + p2Score
+        } else {
+            p2Val.innerText = p2Score
+        }
         return true
     } else if (gameB[0][2].classList.contains("p2") && gameB[1][1].classList.contains("p2") && gameB[2][0].classList.contains("p2")) {
         w = 1
         h3.innerText = "P2 wins!"
+        p2Score += 1
+        if (p2Score < 10) {
+            p2Val.innerText = "0" + p2Score
+        } else {
+            p2Val.innerText = p2Score
+        }
         return true
     } else if (gameB[0][0].classList.contains("p1") && gameB[0][1].classList.contains("p1") && gameB[0][2].classList.contains("p1")) {
         w = 1
         h3.innerText = "P1 wins!"
+        p1Score += 1
+        if (p1Score < 10) {
+            p1Val.innerText = "0" + p1Score
+        } else {
+            p1Val.innerText = p1Score
+        }
         return true
     } else if (gameB[1][0].classList.contains("p1") && gameB[1][1].classList.contains("p1") && gameB[1][2].classList.contains("p1")) {
         w = 1
         h3.innerText = "P1 wins!"
+        p1Score += 1
+        if (p1Score < 10) {
+            p1Val.innerText = "0" + p1Score
+        } else {
+            p1Val.innerText = p1Score
+        }
         return true
     } else if (gameB[2][0].classList.contains("p1") && gameB[2][1].classList.contains("p1") && gameB[2][2].classList.contains("p1")) {
         w = 1
         h3.innerText = "P1 wins!"
+        p1Score += 1
+        if (p1Score < 10) {
+            p1Val.innerText = "0" + p1Score
+        } else {
+            p1Val.innerText = p1Score
+        }
         return true
     } else if (gameB[0][0].classList.contains("p2") && gameB[0][1].classList.contains("p2") && gameB[0][2].classList.contains("p2")) {
         w = 1
         h3.innerText = "P2 wins!"
+        p2Score += 1
+        if (p2Score < 10) {
+            p2Val.innerText = "0" + p2Score
+        } else {
+            p2Val.innerText = p2Score
+        }
         return true
     } else if (gameB[1][0].classList.contains("p2") && gameB[1][1].classList.contains("p2") && gameB[1][2].classList.contains("p2")) {
         w = 1
         h3.innerText = "P2 wins!"
+        p2Score += 1
+        if (p2Score < 10) {
+            p2Val.innerText = "0" + p2Score
+        } else {
+            p2Val.innerText = p2Score
+        }
         return true
     } else if (gameB[2][0].classList.contains("p2") && gameB[2][1].classList.contains("p2") && gameB[2][2].classList.contains("p2")) {
         w = 1
         h3.innerText = "P2 wins!"
+        p2Score += 1
+        if (p2Score < 10) {
+            p2Val.innerText = "0" + p2Score
+        } else {
+            p2Val.innerText = p2Score
+        }
         return true
     } else {
         return false
@@ -110,5 +210,7 @@ function clearBoard () {
         box.classList.remove("p2")
     })
     currentP = "p1"
+    w = 0
+    c = 0
     h3.innerText = "---"
 }
